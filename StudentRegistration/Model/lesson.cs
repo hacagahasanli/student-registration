44d4e451600f35ca1efb27bf3f14e9be3cb1e939
Hacaga <hacaga125@gmail.com>
2022-03-22 23:04:54 +0400

    initial commit

---
+++ StudentRegistration/Model/lesson.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRegistration.Model
+{
+    public class lesson
+    {
+        [Key]
+
+        public  int lId { get; set; }
+
+        [Required]
+        public string lCode { get; set; }
+
+        [Required]
+        public string lName { get; set; }
+
+        [Required]
+        public int lClass { get; set; }
+
+        [Required]
+        public string lTeacherName { get; set; }
+
+        [Required]
+        public int lTeacherSurname { get; set; }
+
+    }
+}
